bb07580767da0be754a88f77814a9e5698b7a49d
LuckyBulgur <61748921+LuckyBulgur@users.noreply.github.com>
2021-05-28 16:18:12 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 sc0tt_lucky_driveby = {}
 
-sc0tt_lucky_driveby.ped = GetPlayerPed(-1) --Bitte nichts ändern
-sc0tt_lucky_driveby.player = PlayerId() --Bitte nichts ändern
+sc0tt_lucky_driveby.ped = GetPlayerPed(-1) --Please dont edit
+sc0tt_lucky_driveby.player = PlayerId() --Please dont edit
 
-sc0tt_lucky_driveby.kmh = 100 -- Bis zu welcher Geschwindigkeit kann man schießen?
+sc0tt_lucky_driveby.kmh = 100 -- Up to wich speed you can shoot
